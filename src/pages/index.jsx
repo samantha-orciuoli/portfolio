b44502a88721d8edb34a020c6f671a6e4e873c1b
samantha-orciuoli <91764744+samantha-orciuoli@users.noreply.github.com>
2022-09-17 01:09:13 -0400

    Changed meta and paragraph text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -255,13 +255,13 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Samantha, a software engineer, developer, and student based in New Jersey. I’m the founder of..."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Samantha Orciuoli, founder, and amateur astronaut.
+            Samantha Orciuoli, software engineer, developer, and student.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Spencer, a software designer and entrepreneur based in New York
