f33a7d5d953d2f94e11664e009a744501a20ba1c
samantha-orciuoli <91764744+samantha-orciuoli@users.noreply.github.com>
2022-10-16 00:11:48 -0400

    updated blog titles

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -41,12 +41,12 @@ export default function BlogsIndex({ blogs }) {
         <title>Blogs - Samantha Orciuoli</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="Writing on my experiences as a software engineer."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on my experiences as a software engineer."
+        intro="Some of my thoughts on programming and my experience exploring a new technological world."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
